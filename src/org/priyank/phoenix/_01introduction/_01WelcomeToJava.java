7a7ff39bf2937a980ae994393895fef0e3e7eb68
priyank uppal <priyank.uppal@gspann.com>
2018-11-20 13:49:49 +0530

    Committing solution for WelcomeToJava

---
+++ src/org/priyank/phoenix/_01introduction/_01WelcomeToJava.java
@@ -0,0 +1,9 @@
+package org.priyank.phoenix._01introduction;
+
+public class _01WelcomeToJava {
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+
+    }
+}
